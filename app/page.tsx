d53352d3bd60778b41a399deb875aa449cea904e
Mitch <63945238+macla7@users.noreply.github.com>
2025-02-02 12:00:37 +1100

    fetch users from aws when auth

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { getCurrentUser } from "aws-amplify/auth";
+import { fetchAuthSession } from "aws-amplify/auth";
 
 export default function Page() {
   const [user, setUser] = useState<any>(null);
@@ -22,13 +23,41 @@ export default function Page() {
     checkUser();
   }, [router]); // Run once on mount
 
+  async function fetchData() {
+    try {
+      const session = await fetchAuthSession();
+      const jwtToken = session.tokens?.idToken?.toString(); // Use ID token
+
+      console.log("sessssssion issss :", session);
+      console.log("jwtToken issss :", jwtToken);
+      const response = await fetch(
+        "https://qf3cucadwb.execute-api.ap-southeast-2.amazonaws.com/users",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer test`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Run once on mount
+
   return (
     <section>
       <h1 className="text-3xl font-bold">Hello world!</h1>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Just making some adjustments, WOOORK!!!!
       </h1>
-
+      <button onClick={fetchData}>Fetch Protected Data</button>;
       {user ? (
         <div>
           <p>Welcome, {user.username}!</p>
