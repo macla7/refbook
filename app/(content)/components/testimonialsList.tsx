1ed63e2e59e9efe9ab1e07db6f34d78ec19ee356
Mitch <63945238+macla7@users.noreply.github.com>
2025-02-07 20:51:50 +1100

    removing most of the blogs stuff

--- app/(content)/components/testimonialsList.tsx
+++ app/(content)/components/testimonialsList.tsx
@@ -40,7 +40,10 @@ export function TestimonialsList() {
     <ul>
       {testimonials.map((testimonal) => (
         <li key={testimonal.id}>
-          <Link className="flex flex-col space-y-1 mb-4" href={`/blog/}`}>
+          <Link
+            className="flex flex-col space-y-1 mb-4"
+            href={`/testimonial/}`}
+          >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="w-[100px] tabular-nums">
                 Author is: {testimonal.authorId}
