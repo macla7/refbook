c6b7c5fc8d84dd46e764aeec9070a7883f42306f
Mitch <63945238+macla7@users.noreply.github.com>
2025-02-24 07:28:19 +1100

    add dp to testimonialCard

--- app/(content)/components/testimonialCard.tsx
+++ app/(content)/components/testimonialCard.tsx
@@ -1,5 +1,6 @@
 import { Testimonial } from "app/types";
 import { useState } from "react";
+import { DP } from "./dp";
 
 export default function TestimonialCard({
   testimonial,
@@ -13,24 +14,11 @@ export default function TestimonialCard({
       {/* Button to Open Modal */}
       <button
         onClick={() => setIsOpen(true)}
-        className="flex flex-col md:flex-row items-center bg-white border border-white shadow-sm hover:bg-gray-100 lg:h-[200px] lg:w-[400px] overflow-hidden"
+        className="flex flex-col md:flex-row items-center bg-white border border-gray-100 rounded-md shadow-sm hover:bg-gray-100 lg:h-[200px] lg:w-[400px] overflow-hidden"
       >
         {/* SVG Container - 1/3 width */}
-        <div className="flex-[1] flex flex-col items-center justify-center bg-gray-100 p-2 w-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-12"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
+        <div className="flex-[1] flex flex-col items-center justify-center p-2 w-full">
+          <DP />
           <p className="text-xs font-medium text-gray-700 w-full text-center">
             {testimonial.authorName}
           </p>
