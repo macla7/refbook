9b4ba3c05bfcd31a0d900b364b8167a5d05e556f
Mitch <63945238+macla7@users.noreply.github.com>
2025-02-27 21:39:11 +1100

    fixing search bar look

--- app/(content)/components/nav.tsx
+++ app/(content)/components/nav.tsx
@@ -25,10 +25,43 @@ export function Navbar() {
         (&nbsp;&nbsp;) (&nbsp;&nbsp;) Champ Stamp
       </h1>
       <div className="flex flex-row items-center">
+        <form className="w-sm mr-8">
+          <div className="flex">
+            <div className="relative w-full">
+              <input
+                type="search"
+                className="p-4 w-full border-none z-20 text-sm text-gray-900 bg-gray-50 rounded-md focus:outline-none focus:ring-2 focus:border-white"
+                placeholder="Search Users Baby..."
+                required
+              />
+              <button
+                type="submit"
+                className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-black rounded-e-md"
+              >
+                <svg
+                  className="w-8 h-4"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+                <span className="sr-only">Search</span>
+              </button>
+            </div>
+          </div>
+        </form>
         {Object.entries(navItems).map(([path, { name }]) => {
           return (
             <Link
-              className="mx-1 rounded-sm bg-our-nav px-6 py-3 my-2 text-lg font-semibold  hover:bg-our-sec focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="mr-8 rounded-md bg-black text-white px-6 py-3 my-2 text-lg font-semibold  hover:bg-our-sec focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               key={path}
               href={path}
             >
