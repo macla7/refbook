dfd20fd21d2f0809dc4b623318b790cf76c2ee1b
Mitch <63945238+macla7@users.noreply.github.com>
2025-04-24 17:00:28 +1000

    background img lava lamp

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -11,6 +11,7 @@ import Link from "next/link";
 import TestimonialCard from "./components/testimonialCard";
 import { Testimonial } from "app/types/testimonial";
 import heroImage from "assets/heropage.jpg";
+import background from "assets/iStock-2163734002-2.svg";
 
 // *** This is the root / landing page ! ***
 export default function rootPage() {
@@ -32,18 +33,20 @@ export default function rootPage() {
   };
 
   return (
-    <>
-      <div className="flex flex-col w-screen h-screen justify-top p-24 bg-[conic-gradient(from_90deg,_var(--color-ourGold2),_var(--color-ourGold))]">
-        <div className="relative inset-y-0 right-0 z-10 flex flex-col p-12 text-right">
-          <p className="text-6xl font-light mb-6">Wrangling with references?</p>
-          <p className="text-6xl font-bold">Let Rango handle it</p>
-        </div>
-        <div className="relative grid grid-cols-3 gap-8 p-12">
-          <TestimonialCard testimonial={fakeTestimony} />
-          <TestimonialCard testimonial={fakeTestimony} />
-          <TestimonialCard testimonial={fakeTestimony} />
-        </div>
+    <div
+      className=" w-full min-h-screen bg-cover bg-center bg-no-repeat overflow-hidden"
+      style={{ backgroundImage: `url(${background.src})` }}
+    >
+      {/* <div className="flex flex-col w-screen h-screen justify-top p-24 bg-[conic-gradient(from_90deg,_var(--color-ourGold2),_var(--color-ourGold))]"> */}
+      <div className="relative inset-y-0 right-0 z-10 flex flex-col p-12 text-right">
+        <p className="text-6xl font-light mb-6">Wrangling with references?</p>
+        <p className="text-6xl font-bold">Let Rango handle it</p>
       </div>
-    </>
+      <div className="relative grid grid-cols-3 gap-8 p-12">
+        <TestimonialCard testimonial={fakeTestimony} />
+        <TestimonialCard testimonial={fakeTestimony} />
+        <TestimonialCard testimonial={fakeTestimony} />
+      </div>
+    </div>
   );
 }
