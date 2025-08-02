c88c28ea84ddac5fb92b73f4806c6b87b687a66c
Mitch <63945238+macla7@users.noreply.github.com>
2025-08-02 14:43:34 +1000

    cleanup account form and sidebar

--- app/(content)/components/sidebar.tsx
+++ app/(content)/components/sidebar.tsx
@@ -38,10 +38,16 @@ export function Sidebar({ user }: { user: User }) {
 
       <h2 className="mt-8 text-2xl/7 font-bold text-gray-900 ">{user.name}</h2>
 
+      <h3 className="mt-8 text-xl/7 font-bold text-gray-900 ">
+        {user.position}
+      </h3>
+      <h3 className=" text-xl/7 font-bold text-gray-900 ">at</h3>
+      <h3 className=" text-xl/7 font-bold text-gray-900 ">{user.workplace}</h3>
+
       {loggedInUser?.userId !== user.id && (
         <Link
           href={`/users/${user.id}/createTestimonial`}
-          className="cursor-pointer rounded-full bg-ourPurple px-6 my-2 text-lg font-semibold transition h-12 flex items-center justify-center "
+          className="mt-8 cursor-pointer rounded-full bg-ourPurple px-6 text-lg font-semibold transition h-12 flex items-center justify-center "
         >
           <span className="relative text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-ourBrown after:transition-all after:duration-1000 hover:after:w-full">
             Make Testimonial
--- app/(content)/users/[id]/account/page.tsx
+++ app/(content)/users/[id]/account/page.tsx
@@ -7,6 +7,8 @@ import { userDefault } from "app/defaults/user";
 import { getUser, patchUser, uploadProfileImage } from "app/api/users";
 import { useRouter } from "next/navigation";
 import { DP } from "app/(content)/components/dp";
+import background from "assets/rangobg4.svg";
+import Image from "next/image";
 
 export default function Page({ params }: { params: { id: string } }) {
   const [user, setUser] = useState<User>(userDefault);
@@ -86,18 +88,22 @@ export default function Page({ params }: { params: { id: string } }) {
   }
 
   return user.id !== "unknown" ? (
-    <div className="w-full p-8 bg-ourCream flex justify-center">
-      {/* <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight m-10">
-        <p>Name: {user.name}</p>
-        <p>Email: {user.email}</p>
-        <p>Position: {user.position}</p>
-        <p>Workplace: {user.workplace}</p>
-        <p>Bio: {user.bio}</p>
-      </h2> */}
+    <div className="w-full ">
+      <div className="col-span-3 justify-items-center rounded-sm bg-white relative p-12">
+        <Image
+          alt="Mountains"
+          src={background}
+          quality={100}
+          fill
+          style={{
+            margin: 0,
+            objectFit: "cover",
+            padding: "0",
+          }}
+        />
 
-      <form className="space-y-12 bg-white p-12 w-3xl rounded-sm">
-        <div className="">
-          <div className="border-b border-gray-900/10 p-12">
+        <form className="relative space-y-2 bg-white w-3xl rounded-sm border border-gray-900/10">
+          <div className="p-12">
             <h2 className="text-base/7 font-semibold text-gray-900">
               User Details
             </h2>
@@ -106,7 +112,7 @@ export default function Page({ params }: { params: { id: string } }) {
               share.
             </p>
 
-            <div className="mt-8 w-64 h-64 rounded-full overflow-hidden border-transparent">
+            <div className="mt-4 w-64 h-64 rounded-full overflow-hidden border-transparent">
               <DP user={{ name: "placeholder", image: imageUrl }} />
             </div>
 
@@ -144,12 +150,12 @@ export default function Page({ params }: { params: { id: string } }) {
                 )}
               </div>
 
-              <p className="mt-3 text-sm/6 text-gray-600">
+              <p className="mt-1 text-sm/6 text-gray-600">
                 Profile pictures help people recognise you
               </p>
             </div>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="col-span-full">
                 <label
                   htmlFor="about"
@@ -168,13 +174,13 @@ export default function Page({ params }: { params: { id: string } }) {
                     value={name}
                   ></textarea>
                 </div>
-                <p className="mt-3 text-sm/6 text-gray-600">
+                <p className="mt-1 text-sm/6 text-gray-600">
                   What is your name?
                 </p>
               </div>
             </div>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="col-span-full">
                 <label
                   htmlFor="about"
@@ -193,13 +199,13 @@ export default function Page({ params }: { params: { id: string } }) {
                     value={position}
                   ></textarea>
                 </div>
-                <p className="mt-3 text-sm/6 text-gray-600">
+                <p className="mt-1 text-sm/6 text-gray-600">
                   What is your position?
                 </p>
               </div>
             </div>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="col-span-full">
                 <label
                   htmlFor="about"
@@ -218,13 +224,13 @@ export default function Page({ params }: { params: { id: string } }) {
                     value={workplace}
                   ></textarea>
                 </div>
-                <p className="mt-3 text-sm/6 text-gray-600">
+                <p className="mt-1 text-sm/6 text-gray-600">
                   Where are you currently working?
                 </p>
               </div>
             </div>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="col-span-full">
                 <label
                   htmlFor="about"
@@ -243,24 +249,24 @@ export default function Page({ params }: { params: { id: string } }) {
                     value={bio}
                   ></textarea>
                 </div>
-                <p className="mt-3 text-sm/6 text-gray-600">
+                <p className="mt-1 text-sm/6 text-gray-600">
                   Tell the world a bit about yourself.
                 </p>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <button
-            className="rounded-sm bg-ourGold px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-egBlue focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            type="button"
-            onClick={handleClick}
-          >
-            Finish
-          </button>
-        </div>
-      </form>
+          <div className="flex items-center justify-end gap-x-6 mr-12 mb-4 mt-0">
+            <button
+              className="cursor-pointer rounded-md bg-ourPurple px-4 py-2 text-white text-sm font-medium hover:bg-indigo-700 transition"
+              type="button"
+              onClick={handleClick}
+            >
+              Finish
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   ) : (
     <div>Redirecting...</div>
